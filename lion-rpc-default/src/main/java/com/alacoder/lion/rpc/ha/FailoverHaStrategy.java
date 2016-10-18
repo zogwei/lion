dd1e45eb115eb6d8dd74c9797a0ab05049afee0d
jimmy <zogwei@gmail.com>
2016-10-18 11:34:18 +0800

    commit

---
+++ lion-rpc-default/src/main/java/com/alacoder/lion/rpc/ha/FailoverHaStrategy.java
@@ -0,0 +1,91 @@
+/**
+ * 版权声明：bee 版权所有 违者必究 2016
+ * Copyright: Copyright (c) 2016 
+ * 
+ * @project_name: lion-rpc-default
+ * @Title: FailoverHaStrategy.java
+ * @Package com.alacoder.lion.rpc.ha
+ * @Description: 
+ * @author jimmy.zhong
+ * @date 2016年10月17日 下午3:54:52
+ * @version V1.0
+ */
+
+package com.alacoder.lion.rpc.ha;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.alacoder.common.exception.LionFrameworkException;
+import com.alacoder.common.exception.LionServiceException;
+import com.alacoder.lion.common.extension.SpiMeta;
+import com.alacoder.lion.common.url.LionURL;
+import com.alacoder.lion.common.url.URLParamType;
+import com.alacoder.lion.common.utils.ExceptionUtil;
+import com.alacoder.lion.common.utils.LoggerUtil;
+import com.alacoder.lion.remote.transport.Request;
+import com.alacoder.lion.remote.transport.Response;
+import com.alacoder.lion.rpc.Referer;
+
+/**
+ * @ClassName: FailoverHaStrategy
+ * @Description: 
+ * @author jimmy.zhong
+ * @date 2016年10月17日 下午3:54:52
+ *
+ */
+
+@SpiMeta(name = "failover")
+public class FailoverHaStrategy<T> extends AbstractHaStrategy<T> {
+
+    protected ThreadLocal<List<Referer<T>>> referersHolder = new ThreadLocal<List<Referer<T>>>() {
+        @Override
+        protected java.util.List<Referer<T>> initialValue() {
+            return new ArrayList<Referer<T>>();
+        }
+    };
+
+    @Override
+    public Response call(Request request, LoadBalance<T> loadBalance) {
+
+        List<Referer<T>> referers = selectReferers(request, loadBalance);
+        if (referers.isEmpty()) {
+            throw new LionServiceException(String.format("FailoverHaStrategy No referers for request:%s, loadbalance:%s", request,
+                    loadBalance));
+        }
+        LionURL refUrl = referers.get(0).getUrl();
+        // 先使用method的配置
+        int tryCount =
+                refUrl.getMethodParameter(request.getMethodName(), request.getParamtersDesc(), URLParamType.retries.getName(),
+                        URLParamType.retries.getIntValue());
+        // 如果有问题，则设置为不重试
+        if (tryCount < 0) {
+            tryCount = 0;
+        }
+
+        for (int i = 0; i <= tryCount; i++) {
+            Referer<T> refer = referers.get(i % referers.size());
+            try {
+                request.setRetries(i);
+                return refer.call(request);
+            } catch (RuntimeException e) {
+                // 对于业务异常，直接抛出
+                if (ExceptionUtil.isBizException(e)) {
+                    throw e;
+                } else if (i >= tryCount) {
+                    throw e;
+                }
+                LoggerUtil.warn(String.format("FailoverHaStrategy Call false for request:%s error=%s", request, e.getMessage()));
+            }
+        }
+
+        throw new LionFrameworkException("FailoverHaStrategy.call should not come here!");
+    }
+
+    protected List<Referer<T>> selectReferers(Request request, LoadBalance<T> loadBalance) {
+        List<Referer<T>> referers = referersHolder.get();
+        referers.clear();
+        loadBalance.selectToHolder(request, referers);
+        return referers;
+    }
+}
---
+++ lion-rpc/src/main/java/com/alacoder/lion/rpc/ha/AbstractLoadBalance.java
@@ -0,0 +1,95 @@
+/**
+ * 版权声明：bee 版权所有 违者必究 2016
+ * Copyright: Copyright (c) 2016 
+ * 
+ * @project_name: lion-rpc
+ * @Title: AbstractLoadBalance.java
+ * @Package com.alacoder.lion.rpc.ha
+ * @Description: 
+ * @author jimmy.zhong
+ * @date 2016年10月14日 下午6:40:32
+ * @version V1.0
+ */
+
+package com.alacoder.lion.rpc.ha;
+
+import java.util.List;
+
+import com.alacoder.common.exception.LionServiceException;
+import com.alacoder.lion.common.utils.LoggerUtil;
+import com.alacoder.lion.remote.transport.Request;
+import com.alacoder.lion.rpc.Referer;
+
+/**
+ * @ClassName: AbstractLoadBalance
+ * @Description: 
+ * @author jimmy.zhong
+ * @date 2016年10月14日 下午6:40:32
+ *
+ */
+
+public abstract class AbstractLoadBalance<T> implements LoadBalance<T> {
+	
+	private List<Referer<T>> referers;
+
+	@Override
+	public void onRefresh(List<Referer<T>> referers) {
+		 // 只能引用替换，不能进行referers update。
+        this.referers = referers;
+	}
+
+	 @Override
+	    public Referer<T> select(Request request) {
+	        List<Referer<T>> referers = this.referers;
+
+	        Referer<T> ref = null;
+	        if (referers.size() > 1) {
+	            ref = doSelect(request);
+
+	        } else if (referers.size() == 1) {
+	            ref = referers.get(0).isAvailable() ? referers.get(0) : null;
+	        }
+
+	        if (ref != null) {
+	            return ref;
+	        }
+	        throw new LionServiceException(this.getClass().getSimpleName() + " No available referers for call request:" + request);
+	    }
+
+
+	@Override
+	public void selectToHolder(Request request, List<Referer<T>> refersHolder) {
+		 List<Referer<T>> referers = this.referers;
+
+	        if (referers == null) {
+	            throw new LionServiceException(this.getClass().getSimpleName() + " No available referers for call : referers_size= 0 "
+	                    +" " + request);
+	        }
+
+	        if (referers.size() > 1) {
+	            doSelectToHolder(request, refersHolder);
+
+	        } else if (referers.size() == 1 && referers.get(0).isAvailable()) {
+	            refersHolder.add(referers.get(0));
+	        }
+	        if (refersHolder.isEmpty()) {
+	            throw new LionServiceException(this.getClass().getSimpleName() + " No available referers for call : referers_size="
+	                    + referers.size() + " " + request);
+	        }
+	    }
+
+
+    protected List<Referer<T>> getReferers() {
+        return referers;
+    }
+
+    @Override
+    public void setWeightString(String weightString) {
+        LoggerUtil.info("ignore weightString:" + weightString);
+    }
+	
+    protected abstract Referer<T> doSelect(Request request);
+
+    protected abstract void doSelectToHolder(Request request, List<Referer<T>> refersHolder);
+
+}
