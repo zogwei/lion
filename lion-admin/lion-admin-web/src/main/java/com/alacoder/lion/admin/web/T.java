fb1d880f93efd1e380dbf0a83305345e35e59bb0
jimmy <zogwei@gmail.com>
2017-01-18 09:52:08 +0800

    add admin ui demo

---
+++ lion-admin/lion-admin-web/src/main/java/com/alacoder/lion/admin/web/T.java
@@ -0,0 +1,26 @@
+/**
+ * 版权声明：lion 版权所有 违者必究 2016
+ * Copyright: Copyright (c) 2016 
+ * 
+ * @project_name: lion-admin-web
+ * @Title: T.java
+ * @Package com.alacoder.lion.admin.web
+ * @Description: 
+ * @author jimmy.zhong
+ * @date 2017年1月17日 下午5:16:13
+ * @version V1.0
+ */
+
+package com.alacoder.lion.admin.web;
+
+/**
+ * @ClassName: T
+ * @Description: 
+ * @author jimmy.zhong
+ * @date 2017年1月17日 下午5:16:13
+ *
+ */
+
+public class T {
+
+}
